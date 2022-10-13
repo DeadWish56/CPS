254f85d3f3b8aa6d7656891249e1fa1a778d1797
DeadWish56 <alekseypluzhnikov@mail.ru>
2022-10-13 20:59:29 +0500

    скрипт для множества кнопок

--- js/swiper.js
+++ js/swiper.js
@@ -40,14 +40,67 @@ window.addEventListener('resize', () => {
   mobileSlider();
 });
 
+
+var buttons = document.querySelectorAll('.section__btn');
+var buttonsIcon = document.querySelectorAll('.btn__expand')
+var sectionsHeight = document.querySelectorAll('.section__inner')
+var buttonsText = document.querySelectorAll('.btn__text')
+
+var clickButton = function (button, sectionHeight, buttonIcon, buttonText) {
+  button.addEventListener('click', function () {
+    icon();
+    height();
+    if (sectionHeight.classList.contains('section__inner--height')) {
+      buttonText.textContent = 'Скрыть'
+    } else {
+      buttonText.textContent = 'Показать все'
+    }
+
+  });
+
+  var height = function () {
+    sectionHeight.classList.toggle('section__inner--height')
+  }
+
+  var icon = function () {
+    buttonIcon.classList.toggle('btn__expand--rotate');
+  }
+
+
+};
+
+
+for (var i = 0; i < buttons.length; i++) {
+  clickButton(buttons[i], sectionsHeight[i], buttonsIcon[i], buttonsText[i]);
+};
+/* для одной секции
+var clickButton = function (button) {
+  button.addEventListener ('click', function () {
+    console.log(button);
+    btnIc();
+  });
+
+  var btnIc = function () {
+  btnIcons.classList.toggle('btn__expand--rotate');
+  console.log(btnIcons)
+
+}
+};
+
+
+for (var i = 0; i < buttons.length; i++) {
+  clickButton(buttons[i])
+  
+  var btnIcons = btnIcon[i]
+
+};
+
+рабочий код для одного элемента
+
 var buttons = document.querySelectorAll('.section__btn');
 var btnIcon = document.querySelectorAll('.btn__expand')
 var sectionsHeight = document.querySelectorAll('.section__inner')
 var buttonsText = document.querySelectorAll('.btn__text')
-
-
-
-
 
 var clickButton = function (button) {
   button.addEventListener('click', function () {
@@ -78,58 +131,4 @@ for (var i = 0; i < buttons.length; i++) {
   var btnIcons = btnIcon[i];
   var buttonText = buttonsText[i]
 };
-
-
-
-
-/* для одной секции
-var clickButton = function (button) {
-  button.addEventListener ('click', function () {
-    console.log(button);
-    btnIc();
-  });
-
-  var btnIc = function () {
-  btnIcons.classList.toggle('btn__expand--rotate');
-  console.log(btnIcons)
-
-}
-};
-
-
-for (var i = 0; i < buttons.length; i++) {
-  clickButton(buttons[i])
-  
-  var btnIcons = btnIcon[i]
-
-};
-
-рабочий код для одного элемента
-
-var clickButton = function (button) {
-  button.addEventListener('click', function () {
-    console.log(button);
-    btnIcon();
-    sectionHeigth();
-  });
-
-var btnIcon = function () {
-  btnIcons.classList.toggle('btn__expand--rotate');
-  console.log(btnIcons)
-}
-
-var sectionHeigth = function () {
-  sectionHeight.classList.toggle('swiper--height')
-}
-
-};
-
-
-for (var i = 0; i < buttons.length; i++) {
-  clickButton(buttons[i]);
-  var sectionHeight = sectionsHeight[i];
-  var btnIcons = btnIcon[i];
-
-};
-
 */
