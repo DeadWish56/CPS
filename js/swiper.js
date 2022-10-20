f18263e32b80b8735c89c25777af6f89dd18570e
DeadWish56 <alekseypluzhnikov@mail.ru>
2022-10-20 12:54:32 +0500

    Правки блока брендов

--- js/swiper.js
+++ js/swiper.js
@@ -1,13 +1,12 @@
 const sliders = document.querySelectorAll('.swiper');
-let mySwiper = null;
+let swiper = null;
 
 function mobileSlider() {
   for (let i = 0; i < sliders.length; i++) {
     let slider = sliders[i];
 
-    if (window.innerWidth < 767 && slider.dataset.mobile == 'false') {
-      mySwiper = new Swiper(slider, {
-        direction: 'horizontal',
+    if (window.innerWidth < 768 && slider.dataset.mobile == 'false') {
+      swiper = new Swiper(slider, {
         loop: true,
         pagination: {
           el: slider.querySelector('.swiper-pagination'),
@@ -20,17 +19,16 @@ function mobileSlider() {
       slider.dataset.mobile = 'true';
     }
 
-    if (window.innerWidth > 767) {
+    if (window.innerWidth > 767 && slider.dataset.mobile == 'true') {
       slider.dataset.mobile = 'false';
 
-      if (slider.classList.contains('swiper-initialized')) {
-        mySwiper.destroy();
+      if (slider.dataset.mobile == 'false') {
+        swiper.destroy();
       }
     }
-    
   }
 }
-    mobileSlider();
-    window.addEventListener('resize', () => {
-      mobileSlider();
-    });
+mobileSlider();
+window.addEventListener('resize', () => {
+  mobileSlider();
+});
