0410b348de7eff5d2e88872971c22243e02de7ac
DeadWish56 <alekseypluzhnikov@mail.ru>
2022-10-24 21:33:37 +0500

    правки свайпера

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,8 +1,45 @@
-const sliders = document.querySelectorAll('.swiper');
-let swiper = null;
+import Swiper from '../../node_modules/swiper/swiper-bundle';
 
+const sliders = document.querySelectorAll('.swiper');
+
+for (let i = 0; i < sliders.length; i++) {
+  let slider = sliders[i];
+  let mySlider;
+  function mobileSlider() {
+    if (window.innerWidth <= 768 && slider.dataset.mobile == 'false') {
+      mySlider = new Swiper(slider, {
+        slidesPerView: "auto",
+        spaceBetween: 16,
+        loop: true,  
+        pagination: {
+          el: slider.querySelector('.swiper-pagination'),
+          clickable: true,
+        },
+      });
+  
+      slider.dataset.mobile = 'true';
+    }
+  
+    if (window.innerWidth > 768 && slider.dataset.mobile == 'true') {
+      slider.dataset.mobile = 'false';
+  
+      if (slider.classList.contains('swiper-initialized')) {
+        mySlider.destroy();
+      }
+    }
+  }
+  
+  mobileSlider();
+  
+  window.addEventListener('resize', function() {
+    mobileSlider();
+  });
+}
+
+/*
 function mobileSlider() {
   for (let i = 0; i < sliders.length; i++) {
+    let swiper;
     let slider = sliders[i];
 
     if (window.innerWidth < 768 && slider.dataset.mobile == 'false') {
@@ -19,11 +56,11 @@ function mobileSlider() {
       slider.dataset.mobile = 'true';
     }
 
-    if (window.innerWidth >= 768 && slider.dataset.mobile == 'true') {
+    if (window.innerWidth > 767) {
       slider.dataset.mobile = 'false';
 
       if (slider.dataset.mobile == 'false') {
-        swiper.disable();
+        swiper.destroy();
       }
     }
   }
@@ -32,3 +69,4 @@ mobileSlider();
 window.addEventListener('resize', () => {
   mobileSlider();
 });
+*/
