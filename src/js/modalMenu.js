2cdd780d2660cbe0949c837fa75a7344d6810b90
DeadWish56 <alekseypluzhnikov@mail.ru>
2022-10-30 21:55:31 +0500

    на пути разделения сущностей

--- src/js/modalMenu.js
+++ src/js/modalMenu.js
@@ -1,19 +1,19 @@
 let openButton = document.querySelector('.btn__icon--burger');
 let closeButton = document.querySelector('.btn__icon--close');
-let modalMenu = document.querySelector('.page__modal-menu');
+let modalMenu = document.querySelector('.modal-menu');
 let overlay = document.querySelector('.modal__wrapper');
 
 openButton.onclick = function () {
-    modalMenu.classList.toggle('page__modal-menu--display')
+    modalMenu.classList.toggle('modal-menu--display')
 }
 
 closeButton.onclick = function () {
-    modalMenu.classList.toggle('page__modal-menu--display')
+    modalMenu.classList.toggle('modal-menu--display')
 }
 
 overlay.addEventListener('click', function (evt) {
     if (evt.target == this) {
-        modalMenu.classList.toggle('page__modal-menu--display')
+        modalMenu.classList.toggle('modal-menu--display')
         console.log(evt)
     }
 })
