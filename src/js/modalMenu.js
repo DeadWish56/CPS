e80d10f3ca08750bc189a38ff123cc61f57a3363
DeadWish56 <alekseypluzhnikov@mail.ru>
2022-11-03 20:58:14 +0500

    модалки 1.6

--- src/js/modalMenu.js
+++ src/js/modalMenu.js
@@ -1,9 +1,11 @@
-import {body} from './helpmodal';
+import { body } from './helpmodal';
 let openButton = document.querySelector('.btn__icon--burger');
 let closeButton = document.querySelector('.btn__icon--close');
 let modalMenu = document.querySelector('.modal-menu');
 let overlay = document.querySelector('.modal__wrapper');
+let modalWindow = document.querySelector('.modal-menu__window')
 
+console.log(modalWindow)
 
 openButton.onclick = function () {
     modalMenu.classList.toggle('modal-menu--display')
@@ -19,6 +21,13 @@ overlay.addEventListener('click', function (evt) {
     if (evt.target == this) {
         modalMenu.classList.toggle('modal-menu--display')
         body.classList.remove('page__body--overflow')
-        console.log(evt)
+    }
+})
+
+body.addEventListener('keydown', function (evt) {
+
+    if (evt.keyCode === 27) {
+        modalMenu.classList.add('modal-menu--display')
+        console.log('ЕСК!')
     }
 })
