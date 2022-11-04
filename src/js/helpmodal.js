35d2f53bc72958801099bb59c7d8b2b134c6520f
DeadWish56 <alekseypluzhnikov@mail.ru>
2022-11-04 19:08:22 +0500

    исправление ошибки js

--- src/js/helpmodal.js
+++ src/js/helpmodal.js
@@ -13,27 +13,29 @@ for (let i = 0; i < helpBtns.length; i++) {
 
         document.querySelector(`[data-target="${path}"]`).classList.add('modal-help__window--display');
         modalHelp.classList.add('modal-help--display')
-        console.log(path)
         body.classList.add('page__body--overflow')
     })
 }
 
-for (let i = 0; i < modalWindows.length; i++) {
+
+for (let i = 0; i < closeModalsHelp.length; i++) {
     let closeModalHelp = closeModalsHelp[i];
-    let modalWindow = modalWindows[i];
 
-    modalOverlay.addEventListener('click', function (evt) {
-        if (evt.target == this) {
+    for (let j = 0; j < modalWindows.length; j++) {
+        let modalWindow = modalWindows[j];
+
+        modalOverlay.addEventListener('click', function (evt) {
+            if (evt.target == this) {
+                modalHelp.classList.remove('modal-help--display')
+                modalWindow.classList.remove('modal-help__window--display')
+                body.classList.remove('page__body--overflow')
+            }
+        })
+
+        closeModalHelp.addEventListener('click', function () {
             modalHelp.classList.remove('modal-help--display')
-            modalWindow.classList.remove('modal-help__window--display')
             body.classList.remove('page__body--overflow')
-        }
-    })
-
-    closeModalHelp.addEventListener('click', function () {
-        modalHelp.classList.remove('modal-help--display')
-        modalWindow.classList.remove('modal-help__window--display')
-        body.classList.remove('page__body--overflow')
-    })
-
+            modalWindow.classList.remove('modal-help__window--display')
+        })
+    }
 }
--- src/js/modalMenu.js
+++ src/js/modalMenu.js
@@ -3,9 +3,6 @@ let openButton = document.querySelector('.btn__icon--burger');
 let closeButton = document.querySelector('.btn__icon--close');
 let modalMenu = document.querySelector('.modal-menu');
 let overlay = document.querySelector('.modal__wrapper');
-let modalWindow = document.querySelector('.modal-menu__window')
-
-console.log(modalWindow)
 
 openButton.onclick = function () {
     modalMenu.classList.toggle('modal-menu--display')
@@ -25,9 +22,7 @@ overlay.addEventListener('click', function (evt) {
 })
 
 body.addEventListener('keydown', function (evt) {
-
     if (evt.keyCode === 27) {
         modalMenu.classList.add('modal-menu--display')
-        console.log('ЕСК!')
     }
 })
