fe6f3b03a6986f42bb3d548b77e58a54aa1dc5e2
DeadWish56 <alekseypluzhnikov@mail.ru>
2022-11-08 20:28:55 +0700

    модалки с помощью делегирования

--- src/js/helpmodal.js
+++ src/js/helpmodal.js
@@ -1,41 +1,34 @@
-const helpBtns = document.querySelectorAll('.btn-modal');
-const modalHelp = document.querySelector('.modal-help');
-const modalOverlay = document.querySelector('.modal-help__overlay');
-const modalWindows = document.querySelectorAll('.modal-help__window');
+const modalHelp = document.querySelector('.modal-help')
+const modalsWindows = document.querySelectorAll('.modal-help__window')
 export const body = document.querySelector('.page__body');
-const closeModalsHelp = document.querySelectorAll('.modal-help__close-btn');
 
-for (let i = 0; i < helpBtns.length; i++) {
-    let helpBtn = helpBtns[i];
-
-    helpBtn.addEventListener('click', function () {
-        let path = this.getAttribute('data-path');
-
-        document.querySelector(`[data-target="${path}"]`).classList.add('modal-help__window--display');
-        modalHelp.classList.add('modal-help--display')
-        body.classList.add('page__body--overflow')
-    })
+const openModalHelp = function () {
+    body.classList.add('page__body--overflow')
+    modalHelp.classList.add('modal-help--display')
 }
 
+const closeModalHelp = function () {
+    for (let i = 0; i < modalsWindows.length; i++) {
+        let modalWindow = modalsWindows[i];
 
-for (let i = 0; i < closeModalsHelp.length; i++) {
-    let closeModalHelp = closeModalsHelp[i];
-
-    for (let j = 0; j < modalWindows.length; j++) {
-        let modalWindow = modalWindows[j];
-
-        modalOverlay.addEventListener('click', function (evt) {
-            if (evt.target == this) {
-                modalHelp.classList.remove('modal-help--display')
-                modalWindow.classList.remove('modal-help__window--display')
-                body.classList.remove('page__body--overflow')
-            }
-        })
-
-        closeModalHelp.addEventListener('click', function () {
+        if (modalWindow.classList.contains('modal-help__window--display')) {
+            modalWindow.classList.remove('modal-help__window--display')
             modalHelp.classList.remove('modal-help--display')
             body.classList.remove('page__body--overflow')
-            modalWindow.classList.remove('modal-help__window--display')
-        })
+        }
     }
 }
+
+document.addEventListener('click', function (evt) {
+    const btnOpen = evt.target.dataset.path;
+    const closeModal = evt.target.dataset.name;
+
+    if (btnOpen) {
+        openModalHelp()
+        document.querySelector(`[data-target="${btnOpen}"]`).classList.add('modal-help__window--display');
+    }
+
+    if (closeModal === "close" || closeModal === "overlay") {
+        closeModalHelp();
+    }
+})
