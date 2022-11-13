2892ecd183d181cdf3fe4111a927643cf09122e1
DeadWish56 <alekseypluzhnikov@mail.ru>
2022-11-13 21:55:44 +0700

    перед ревью

--- src/js/helpmodal.js
+++ src/js/helpmodal.js
@@ -18,15 +18,23 @@ const closeModalHelp = function () {
 }
 
 document.addEventListener('click', function (evt) {
-    const btnOpen = evt.target.dataset.path;
     const closeModal = evt.target.dataset.name;
+    const btnOpen = evt.target.dataset.path;
 
-    if (btnOpen) {
-        openModalHelp()
-        document.querySelector(`[data-target="${btnOpen}"]`).classList.add('modal-help__window--display');
+    if (evt.target.closest('.btn')) {
+        if (btnOpen) {
+            openModalHelp()
+            document.querySelector(`[data-target="${btnOpen}"]`).classList.add('modal-help__window--display');
+        }
+
+        if (closeModal === "close") {
+            closeModalHelp();
+        }
     }
 
-    if (closeModal === "close" || closeModal === "overlay") {
-        closeModalHelp();
+    if (evt.target.closest('.modal-help__overlay')) {
+        if (closeModal === "overlay") {
+            closeModalHelp();
+        }
     }
 })
